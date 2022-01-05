3db07586a5489390b52366633d5c4fb19d2d9b31
kevinchengkac <96162845+kevinchengkac@users.noreply.github.com>
2022-01-04 23:50:04 -0500

    Project 2 - Covid-19 vs. Influenza Cases
    As a laboratory assistant, my coworkers and I noticed that there were a sudden decrease in influenza cases. I compared Covid-19 deaths to Influenza deaths as well as the activity level to determine if masks were the reason for it. The code in comments are code that is not used for the final tableau project.

--- Project2.sql
+++ Project2.sql
@@ -1,5 +1,5 @@
---Selects all data from 2018-2021
-Select * From InfluenzaStateLevels
+/**--Selects all data from 2018-2021
+Select * From InfluenzaStateLevels**/
 
 --Selects data based on US' influenza activity level
 --Orders by earliest season and date
@@ -8,7 +8,7 @@ From InfluenzaStateLevels
 Order By Year ASC, week ASC;
 
 ------------------State Examples--------------------------------------------------------
---Using Alabama as an example for how flu cases weakened throughout
+/**--Using Alabama as an example for how flu cases weakened throughout
 --2020 as masks mandates start to occur due to the spread of Covid-19
 Select state, activity_level, weekend
 From InfluenzaStateLevels
@@ -20,7 +20,7 @@ Order By activity_level DESC;
 Select state, activity_level, activity_level_label, weekend, week, season, date 
 From InfluenzaStateLevels
 Where state = 'Florida'
-Order By date ASC;
+Order By date ASC;**/
 
 ---------------------------------------------------Deaths in the United States----------------------------------------------
 --Lists all data of Covid, Pneumonia, and Influenza death cases in NA
@@ -37,7 +37,7 @@ Where age_group = 'All Ages'
 Order By date ASC; 
 
 ---------------Combining the datasets together...?--------------------------------------------------------------------------
---Trying to put both datasets together
+/**--Trying to put both datasets together
 --Doesn't seem to work well; need to push start_week column down to match with 2019-2021 data
 --Might not be possible to combine. Will leave code for now, but will probably have to use two 
 --separate tables for the tableau project.
@@ -46,5 +46,5 @@ dea.date, IsNull(dea.c19_deaths, 0) As c19_deaths, IsNull(dea.flu_deaths, 0) as 
 	From PortfolioProject..InfluenzaStateLevels As flu
     Join PortfolioProject..us_c19_flu_pnu_deaths As dea
 		On flu.state = dea.state
-		Order By flu.year ASC, flu.week ASC;
+		Order By flu.year ASC, flu.week ASC;**/
 
